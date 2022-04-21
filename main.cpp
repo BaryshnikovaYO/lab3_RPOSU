96cd896df95770d372c9b96daabce7d59d4a4a1c
Yulia <ulia29042004@yandex.ru>
2022-04-21 22:16:20 +0300

    code: без функции поиска мин и макс

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "histogram.h"
 
 using namespace std;
 
@@ -14,24 +15,6 @@ vector<double> input_numbers(size_t count)
         cin >> result[i];
     }
     return result;
-}
-
-void find_minmax(const vector<double>& numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers[0];
-
-    for (double number : numbers)
-    {
-        if (min > number)
-        {
-            min = number;
-        }
-        else if (max < number)
-        {
-            max = number;
-        }
-    }
 }
 
 vector<size_t> make_histogram(const vector<double>& numbers, size_t bin_count)
