b463a35da9530baf4181bf791d48c7cee0b318ee
Yulia <ulia29042004@yandex.ru>
2022-05-28 16:08:07 +0300

    code: лаба3

--- histogram.cpp
+++ histogram.cpp
@@ -23,12 +23,3 @@ void find_minmax(const vector<double>& numbers, double& min, double& max)
     }
 }
 
-vector <size_t> procent (size_t number_count, const vector <size_t> bins)
-{
-        vector <size_t> x (bins.size());
-        for ( size_t i = 0; i < bins.size(); i++ )
-        {
-            x[i] = static_cast<double> (bins[i]) / number_count * 100;
-        }
-        return x;
-}
